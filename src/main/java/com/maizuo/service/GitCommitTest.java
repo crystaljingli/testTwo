982ab19a9958fd2acfee3f9e0692e9318f7ceb0c
crystal <crystal@hyx.com>
2016-05-05 19:18:05 +0800

    aaaa

--- src/main/java/com/maizuo/service/GitCommitTest.java
+++ src/main/java/com/maizuo/service/GitCommitTest.java
@@ -1,8 +1,13 @@
 package com.maizuo.service;
 
+import com.maizuo.bean.GitTest;
+
 public class GitCommitTest {
   public static void main(String[] args) {
 	//此处代码留有另一个人编写（E://workspace）
+	  GitTest gitTest = new GitTest();
+	  gitTest.setGitName("ysyyy");
 	  System.out.println("This is a Tested!");
+	  
  }
 }
