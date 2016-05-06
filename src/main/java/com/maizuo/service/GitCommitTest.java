f4896b8922fd28c27f69417b6dca17ddd1395999
crystal <crystal@hyx.com>
2016-05-06 15:53:46 +0800

    ttt

--- src/main/java/com/maizuo/service/GitCommitTest.java
+++ src/main/java/com/maizuo/service/GitCommitTest.java
@@ -1,15 +1,20 @@
 package com.maizuo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.maizuo.bean.GitTest;
 
 public class GitCommitTest {
   public static void main(String[] args) {
+	  List<GitTest> gitTestList = new ArrayList<GitTest>();
 	//此处代码留有另一个人编写（E://workspace）
 	  GitTest gitTest = new GitTest();
 	  gitTest.setGitName("ysyyy");
 	//此处代码由（E://workspace）完成
 	  gitTest.setGitPrice("12345");
 	  gitTest.setGitVersion("11");
+	  gitTestList.add(gitTest);
 	  System.out.println("This is a Test!");
 	  
  }
